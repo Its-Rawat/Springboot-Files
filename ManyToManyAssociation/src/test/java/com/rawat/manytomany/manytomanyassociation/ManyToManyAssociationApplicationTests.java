bb52e20b46c2e40961b5e4fd79a24ce5d2d776aa
Its-Rawat <adi2002rawat@gmail.com>
2024-09-27 17:32:29 +0530

    Spring ManyToMany Mapping/Association First Commit, Not ready Project

---
+++ ManyToManyAssociation/src/test/java/com/rawat/manytomany/manytomanyassociation/ManyToManyAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawat.manytomany.manytomanyassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyAssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
